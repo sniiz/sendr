67ba191a9df93ffa4edea0d01508824d0265384d
sniiz <88880069+sniiz@users.noreply.github.com>
2022-05-08 10:44:41 +0300

    how does tauri work

---
+++ components/AutoAlert.js
@@ -0,0 +1,12 @@
+import { Alert, Platform } from "react-native";
+import message from "@tauri-apps/api/dialog";
+
+function AutoAlert(msg, title = null) {
+    if (Platform.OS === "ios" || Platform.OS === "android") {
+        Alert.alert(title ? title : "", msg);
+    } else if (Platform.OS !== "web") {
+        message(msg);
+    } else {
+        alert(msg);
+    }
+}
